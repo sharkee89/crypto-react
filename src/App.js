addf3eee2fad56877c8f8784ade0cedfda159def
D065346 <stefan.nikolic@sap.com>
2019-10-28 16:44:28 +0100

    Add basic hand date transformation

--- src/App.js
+++ src/App.js
@@ -18,16 +18,17 @@ class App extends Component {
   componentDidMount() {
     axios.get(`${Config.domain}/cryptocurrencies/BTC/prices`)
       .then(res => {
+        let sdata = {
+          id: res.data[0].name,
+          color: 'hsl(1, 70%, 50%)',
+          data: res.data.map(price => {
+            return {x: `${new Date(price.date).getDate()}.${new Date(price.date).getMonth() + 1}.${new Date(price.date).getFullYear()} ${new Date(price.date).getHours()}:${new Date(price.date).getMinutes()}`, y: price.price}
+          })
+        };
         this.setState({
           cryptocurrency: {name: res.data[0].name, prices: res.data},
           isLoading: false,
-          graphData: [{
-            id: res.data[0].name,
-            color: 'hsl(1, 70%, 50%)',
-            data: res.data.map(price => {
-              return {x: price.date, y: price.price}
-            })
-          }]
+          graphData: [sdata]
         });
       });
   }
@@ -36,6 +37,13 @@ class App extends Component {
     this.setState({isLoading: true});
     axios.get(`${Config.domain}/cryptocurrencies/${symbol}/prices`)
       .then(res => {
+        let sdata = {
+          id: res.data[0].name,
+          color: 'hsl(1, 70%, 50%)',
+          data: res.data.map(price => {
+            return {x: `${new Date(price.date).getDate()}.${new Date(price.date).getMonth() + 1}.${new Date(price.date).getFullYear()} ${new Date(price.date).getHours()}:${new Date(price.date).getMinutes()}`, y: price.price}
+          })
+        };
         this.setState({ 
           navbarItems: this.state.navbarItems.map(item => {
             item.selected = item.symbol === symbol;
@@ -43,13 +51,7 @@ class App extends Component {
           }),
           cryptocurrency: {name: res.data[0].name, symbol: symbol, prices: res.data},
           isLoading: false,
-          graphData: [{
-            id: res.data[0].name,
-            color: 'hsl(1, 70%, 50%)',
-            data: res.data.map(price => {
-              return {x: price.date, y: price.price}
-            })
-          }]
+          graphData: [sdata]
         });
       })
   }
